0236c0ce90760fb74e5c57e2d9dfd085b19bcb2f
Weston Platter <westonplatter@gmail.com>
2017-11-28 00:01:35 -0700

    sidekiq, add a slack queue

--- app/jobs/ping_slack_job.rb
+++ app/jobs/ping_slack_job.rb
@@ -1,6 +1,9 @@
 class PingSlackWorker
   include Sidekiq::Worker
 
+  sidekiq_options \
+    :queue => :slack
+
   def perform(msg)
     client = Slack::Web::Client.new
     client.chat_postMessage(channel: '#pt-production-nxs', text: msg, as_user: true)
