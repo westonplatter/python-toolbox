d89aaa6e616d5720315f2e70d99cb048061131d1
Weston Platter <westonplatter@gmail.com>
2017-11-29 10:36:35 -0700

    haha

--- app/jobs/ping_slack_job.rb
+++ app/jobs/ping_slack_job.rb
@@ -6,6 +6,6 @@ class PingSlackWorker
 
   def perform(msg)
     client = Slack::Web::Client.new
-    client.chat_postMessage(channel: '#pt-production-nxs', text: msg, as_user: true)
+    client.chat_postMessage(channel: '#pt-production', text: msg, as_user: true)
   end
 end
